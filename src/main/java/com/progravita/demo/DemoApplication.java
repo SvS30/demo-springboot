b5a1606d8af59a9226591a5a70c1c21efa14184b
salimv <salimvzqz@gmail.com>
2024-06-08 08:56:24 -0600

    :wrench: Use Dotenv

--- src/main/java/com/progravita/demo/DemoApplication.java
+++ src/main/java/com/progravita/demo/DemoApplication.java
@@ -3,10 +3,16 @@ package com.progravita.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+			.ignoreIfMalformed()
+			.ignoreIfMissing().load();
+		System.out.println("Initializing on " + dotenv.get("APP_DEBUG") + " with jdbc:mysql://" + dotenv.get("DB_HOST") + ":" + dotenv.get("DB_PORT") + "/" + dotenv.get("DB_DATABASE") + "?useSSL=false&serverTimezone=UTC");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
